d7c83bb70f1102eb5b281fe872caa9262c0b9d7f
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-21 20:14:03 +0300

    Update FamilyTree.cpp

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -12,14 +12,14 @@ namespace family{
     Tree& Tree::addFather(const std::string sonName, const std::string fatherName) {
 	    Node sonNode = recursiveGetAncestorNode(this->root , sonName);
 		Node* father = new Node(fatherName);
-        &father->child = &sonNode;
-        &father->rank = &sonNode->rank+1;
+        &father->child = *sonNode;
+        &father.rank = *sonNode->rank+1;
         &father->name = fatherName;
-        if(&father->rank==1)
+        if(&father.rank==1)
             &father.relation = "father";
-        else if(&father->rank==2)
+        else if(&father.rank==2)
             &father.relation = "grandfather";
-        else if(&father->rank>2){
+        else if(&father.rank>2){
             &father.relation = "grandfather";
             std::string tmpName = "";
             for (int i = 2; i < &father.rank; i++){
