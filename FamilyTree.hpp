49538764393166667d8232435b5a9b6151b53a38
Eliot100 <46245107+Eliot100@users.noreply.github.com>
2020-04-22 18:32:16 +0300

    Update FamilyTree.hpp

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -11,7 +11,7 @@ namespace family {
             std::string name;
             std::string relation;
             family::Node *mother, *father, *child;
-	    Node(const std::string nodeName)
+	    Node(std::string nodeName)
 	    : rank(-1), name(""),  relation(""), mother(nullptr), father(nullptr), child(nullptr)
 	    {}
     };
@@ -19,27 +19,22 @@ namespace family {
 	class Tree {
 		public:
 			family::Node* root;
-			Tree (const std::string rootName)
+			Tree (std::string rootName)
 			: root(new Node(rootName))
 			{
 				root->relation = "me";
         			this->root->rank=0;
 			}
-			Tree& addFather (const std::string sonName, const std::string fatherName);
-			Tree& addMother (const std::string sonName, const std::string motherName);
-			Tree& remove (const std::string name) ;
-			std::string find (const std::string relation2Root);
-			std::string relation (const std::string ancestorName);
+			Tree& addFather (std::string sonName, std::string fatherName);
+			Tree& addMother (std::string sonName, std::string motherName);
+			Tree& remove (std::string name) ;
+			std::string find (std::string relation2Root);
+			std::string relation (std::string ancestorName);
 			void display ();
 	};
-    Tree::Tree(const std::string rootName) {
-	this->root = new Node(rootName);
-        root->relation = "me";
-        this->root->rank=0; 
-    }
 	
 }
 void printInorder(family::Node* root);	
-void tokenize(std::string const &str, const char delim, std::vector<std::string> &out);
+void tokenize(std::string &str, const char delim, std::vector<std::string> &out);
 std::string findTemtom(int t, family::Node* temp, std::string out);
-family::Node* recursiveGetAncestorNode(family::Node &rootNode, const std::string ancestorName);
+family::Node* recursiveGetAncestorNode(family::Node &rootNode, std::string ancestorName);
